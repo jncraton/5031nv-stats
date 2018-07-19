f4a9ee28e37a11e18a704b0400ca0827d425c9b1
Jon Craton <jncraton@gmail.com>
2018-07-19 09:54:41 -0400

    Print stats

--- stats.py
+++ stats.py
@@ -1,8 +1,10 @@
-import mechanicalsoup
 import re
+import time
+
+import mechanicalsoup
 
 def get_hosts():
-  hosts = []
+  hosts = {}
 
   col_names = [
     "name","mac","ssid","rssi",
@@ -21,8 +23,29 @@ def get_hosts():
     
       host = {n: cols[i][0] for i,n in enumerate(col_names)}
       if re.match("..:..:..:..:..:..", host['mac']):
-        hosts.append(host)
+        hosts[host['name']] = host
 
   return hosts
 
-print(get_hosts())
+if __name__ == '__main__':
+  print("Getting start state...")
+  start = get_hosts()
+
+  interval = 60
+  
+  for secs in range(interval,0,-5):
+    print("Waiting %d seconds..." % secs)
+    time.sleep(5)
+    
+  print("Getting final state...")
+  end = get_hosts()
+  print("")
+
+  for host in end.values():
+    name = host['name']
+    deltas = {n: int(end[name][n]) - int(start[name][n]) for n in ['rx_bytes','tx_bytes']}
+  
+    print(name)
+    print("%.2f KiBps rx" % (deltas['rx_bytes'] / interval / 1024,))
+    print("%.2f KiBps tx" % (deltas['tx_bytes'] / interval / 1024,))
+    print("")
